f36a4788cd3506ca742ec6c8b0f9d7a077423c1d
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2014-12-14 17:00:13 +0800

    Edited nbaservice to dynamo

--- model/income.rb
+++ model/income.rb
@@ -1,6 +1,17 @@
-require 'sinatra'
-require 'sinatra/activerecord'
-require_relative '../config/environments'
+require 'aws-sdk'
 
 class Income < ActiveRecord::Base
+  string_attr :teamname
+  string_attr :playername1
+  string_attr :playername2
+  string_attr :description
+  timestamps
+
+  def self.destroy(id)
+    find(id).delete
+  end
+
+  def self.delete_all
+    all.each { |r| r.delete }
+  end
 end
